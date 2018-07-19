d6abdf06848bfcf4cf7df8a3f58aef0564bbfaa7
danielfranze <daniel.franze.office@gmail.com>
2018-07-20 01:35:33 +0200

    Add mainmenu

--- src/components/mainmenu.jsx
+++ src/components/mainmenu.jsx
@@ -1,30 +1,54 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Icon, Header, Dropdown, DropdownItem, DropdownDivider } from 'semantic-ui-react'
+import '../sass/main.sass';
+
 
 
 export default class MainMenu extends Component {
-  state = {}
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  constructor(props){
+    super(props);
+      
+    this.state = {
+      disabled: false
+    }
+  }
 
   render() {
+
     const { activeItem } = this.state
 
     return (
-      <Menu>
-        <Menu.Item header>Our Company</Menu.Item>
-        <Menu.Item
-          name='aboutUs'
-          active={activeItem === 'aboutUs'}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item name='jobs' active={activeItem === 'jobs'} onClick={this.handleItemClick} />
-        <Menu.Item
-          name='locations'
-          active={activeItem === 'locations'}
-          onClick={this.handleItemClick}
-        />
-      </Menu>
+
+    <Menu borderless stackable inverted color='green' size='medium' >
+    
+      <Menu.Item  position="left" className='link' href='https://github.com/danielfranze/RhythmCreator'>
+          <Icon disabled={this.state.disabled} name='github' size='huge'></Icon>
+      </Menu.Item>
+
+      <Menu.Item position="left">
+        <Header as='h1' underline='false'>
+          <span class='header_title_left'>Rhythm</span><span class='header_title_right'> creator</span> 
+        </Header>
+      </Menu.Item>
+
+      <Dropdown  position="right" icon='th huge' pointing className='link item' >
+        <Dropdown.Menu >
+          <Dropdown.Header>File</Dropdown.Header>
+          <Dropdown.Item>Save</Dropdown.Item>
+          <Dropdown.Item>Load</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Header>Options</Dropdown.Header>
+          <Dropdown.Item>Reset</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+    </Menu>
+
+
+
+
+
+
     )
   }
 }
