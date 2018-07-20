bc1d3e5da9c530e3ec29d02fba278f212e83d67e
danielfranze <daniel.franze.office@gmail.com>
2018-07-20 18:14:59 +0200

    Add mainmenu modal

--- src/components/mainmenu.jsx
+++ src/components/mainmenu.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import { Menu, Icon, Header, Dropdown, DropdownItem, DropdownDivider } from 'semantic-ui-react'
+import { Menu, Icon, Header, Dropdown, Modal} from 'semantic-ui-react'
 import '../sass/main.sass';
-
-
+import MainMenuModal from './mainmenu_modal.jsx';
 
 export default class MainMenu extends Component {
   constructor(props){
@@ -19,35 +18,29 @@ export default class MainMenu extends Component {
 
     return (
 
-    <Menu borderless stackable inverted color='green' size='medium' >
+    <Menu borderless stackable inverted size='massive' >
     
       <Menu.Item  position="left" className='link' href='https://github.com/danielfranze/RhythmCreator'>
           <Icon disabled={this.state.disabled} name='github' size='huge'></Icon>
       </Menu.Item>
 
-      <Menu.Item position="left">
+      <Menu.Item position="center">
         <Header as='h1' underline='false'>
           <span class='header_title_left'>Rhythm</span><span class='header_title_right'> creator</span> 
         </Header>
       </Menu.Item>
 
-      <Dropdown  position="right" icon='th huge' pointing className='link item' >
+      
+
+      <Dropdown  width="100px" position="right" icon='th huge' pointing='true top right' className='link item' >
         <Dropdown.Menu >
-          <Dropdown.Header>File</Dropdown.Header>
-          <Dropdown.Item>Save</Dropdown.Item>
-          <Dropdown.Item>Load</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Header>Options</Dropdown.Header>
-          <Dropdown.Item>Reset</Dropdown.Item>
+          <Dropdown.Item icon='folder' text='Open'/>
+          <Dropdown.Item icon='file' text='Save'/>
+          <MainMenuModal />
         </Dropdown.Menu>
       </Dropdown>
 
     </Menu>
-
-
-
-
-
 
     )
   }
